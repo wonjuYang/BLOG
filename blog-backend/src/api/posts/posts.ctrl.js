7f6aa6d944fe38967e0f09865a45e1b3f2ed9b09
triplej <his1monologue@gmail.com>
2020-07-29 13:57:12 +0900

    페이지내이션 구현

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -153,9 +153,34 @@ exports.write = async (ctx) => {
 
 
 exports.list = async (ctx) => {
+
+
+
+    //page가 주어지지 않았다면 1로 간주
+    //query는 문자열 형태로 받아 오므로 숫자로 변환
+    const page = parseInt(ctx.query.page || 1, 10);
+
+    //잘못된 페이지가 주어졌다면 오류
+    if(page < 1){
+        ctx.status = 400;
+        return;
+    }
     try{
-        const posts = await Post.find().exec();
-        ctx.body = posts;
+        const posts = await Post.find()
+            .sort({_id:-1})
+            .limit(10)
+            .skip((page-1) * 10)
+            .exec();
+        const postCount = await Post.count().exec();
+        //마지막 페이지알려주기
+        //ctx.set은 response header를 설정
+        ctx.set('Last-Page' , Math.ceil(postCount/10)); 
+
+        const limitBodyLength = post => ({
+            ...post.toJSON(),
+            body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+        });
+        ctx.body = posts.map(limitBodyLength);
     }catch(e){
         ctx.throw(e, 500);
     }
