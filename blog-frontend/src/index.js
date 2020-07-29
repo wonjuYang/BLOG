f5047bff33c064158fbdc78c8e572839be6abc98
triplej <his1monologue@gmail.com>
2020-07-29 17:47:26 +0900

    초기 설정(sass, node_path 등)

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Root from './Root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<Root />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
